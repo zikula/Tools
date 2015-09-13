35b90783bb10086931d3c54d13fd12510fd99e87
Craig Heydenburg <craigh@mac.com>
2015-09-12 20:19:19 -0400

    accomodate 'assign' in various calls.

--- Zikula/Tools/Converter/ZikulaConverter.php
+++ Zikula/Tools/Converter/ZikulaConverter.php
@@ -19,6 +19,8 @@ use Zikula\Tools\ConverterAbstract;
  */
 class ZikulaConverter extends ConverterAbstract
 {
+    private $delims = [['{{', '}}'], ['{%', '%}']];
+
     public function convert(\SplFileInfo $file, $content)
     {
         $content = $this->replaceBlockPosition($content);
@@ -35,9 +37,11 @@ class ZikulaConverter extends ConverterAbstract
     private function replaceBlockPosition($content)
     {
         return preg_replace_callback(
-            "/\{blockposition name=([\w]+)\}/i",
+            "/\{blockposition name=['|\"]?([\w]+)['|\"]?[\s]*(assign=['|\"]?([a-z]+)['|\"]?)?\}/i",
             function ($matches) {
-                return "{{ showblockposition('$matches[1]') }}";
+                $set = (!empty($matches[3])) ? "set $matches[3] = " : '';
+                $delims = $this->delims[(int)!empty($matches[3])];
+                return "$delims[0] {$set}showblockposition('$matches[1]') $delims[1]";
             },
             $content);
     }
@@ -56,9 +60,9 @@ class ZikulaConverter extends ConverterAbstract
     private function replacePageaddvar($content)
     {
         return preg_replace_callback(
-            "/\{pageaddvar name=['|\"]?([\w]+)['|\"]?\svalue=['|\"]?([a-z0-9:_][^'|\"]+)['|\"]?\}/i",
+            "/\{pageaddvar name=['|\"]?([\w]+)['|\"]?\svalue=['|\"]?([a-z0-9$:_][^'|\"]+)['|\"]?\}/i",
             function ($matches) {
-                return "{{ pageAddVar('$matches[1], '') }}{# @todo oldpath= $matches[2] #}";
+                return "{{ pageAddVar('$matches[1]', '') }}{# @todo oldpath= $matches[2] to @VendorBundleTheme:path/from/Resources #}";
             },
             $content);
     }
@@ -67,9 +71,11 @@ class ZikulaConverter extends ConverterAbstract
     {
         // only replaces gt and does not accommodate string replacements or plurals or counts (__f(), __
         return preg_replace_callback(
-            "/\{gt text=['|\"]?([\w][^'|\"]+)['|\"]?['|\"]?\}/i",
+            "/\{gt text=['|\"]?([\w][^'|\"]+)['|\"]?['|\"]?[\s]*(assign=['|\"]?([a-z]+)['|\"]?)?\}/i",
             function ($matches) {
-                return "{{ __('$matches[1]') }}";
+                $set = (!empty($matches[3])) ? "set $matches[3] = " : '';
+                $delims = $this->delims[(int)!empty($matches[3])];
+                return "$delims[0] {$set}__('$matches[1]') $delims[1]";
             },
             $content);
     }
@@ -79,8 +85,9 @@ class ZikulaConverter extends ConverterAbstract
         return preg_replace_callback(
             "/\{checkpermission[\s]+component=['|\"]?([a-z0-9:_]+)['|\"]?[\s]+instance=['|\"]?([a-z0-9:_]+)['|\"]?[\s]+level=['|\"]?([a-z0-9:_]+)['|\"]?[\s]*(assign=['|\"]?([a-z]+)['|\"]?)?(\})/i",
             function ($matches) {
-                $set = (!empty($matches[4])) ? "set $matches[4] = " : '';
-                return "{{ {$set}hasPermission('$matches[1]', '$matches[2]', '$matches[3]') }}{# @todo consider `if hasPermission()` #}";
+                $set = (!empty($matches[5])) ? "set $matches[5] = " : '';
+                $delims = $this->delims[(int)!empty($matches[5])];
+                return "$delims[0] {$set}hasPermission('$matches[1]', '$matches[2]', '$matches[3]') $delims[1]{# @todo consider `if hasPermission()` #}";
             },
             $content);
     }
@@ -111,6 +118,9 @@ class ZikulaConverter extends ConverterAbstract
             "{{ content }}" => "{{ content|raw }}",
             "{{ maincontent }}" => "{{ maincontent|raw }}",
             "{\/checkpermissionblock}" => "{% endif %}",
+            "{nocache}" => "",
+            "{\/nocache}" => "",
+            "{pagerendertime}" => "",
         ];
         foreach ($replacements as $k => $v) {
             $content = preg_replace('/' . $k . '/i', $v, $content);
